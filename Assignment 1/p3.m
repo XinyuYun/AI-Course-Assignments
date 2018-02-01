a2ddd8b66c345c7cc01fc5d9f4dc15902ba334ea
XinyuYun <xyun@uwo.ca>
2018-02-01 17:43:11 -0500

    update

--- Assignment 1/p3.m
+++ Assignment 1/p3.m
@@ -7,9 +7,20 @@ function C = p3(X_train, Y_train, X_test, k)
 % Outputs   vector C stores the classes assigned to the test samples
 % ====================== My IMPLEMENTATION HERE ===========================
 %initialize vector C
-C = zeros(size(X_test, 1));
+C = zeros(size(X_test, 1), 1);
 %compute distances(without sqrt) from each element in X_test to all elements in X_train
-distance_matrix = X_test.^2 * (X_train.^2)'
+for row = 1:size(X_test, 1)
+    %repeat current X_test row to match row size of X_train
+    curr_test_matrix = repmat(X_test(row, :), size(X_train, 1), 1); 
+    %calculate the distance(without sqrt)
+    distance = sum(abs(curr_test_matrix - X_train).^2, 2);
+    %sort the distance result    
+    [Y, I] = sort(distance);
+    %get top k labels from Y_train, and use mode function to get most
+    %frequent label then save it to C(row)
+    C(row) = mode(Y_train(I(1:k)));
+end
+
 
 %
 
